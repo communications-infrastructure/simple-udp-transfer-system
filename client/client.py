e0ec14df27910b42051fb1a2c90db21f72d33e6a
Juan Romero <juan.romero1201@gmail.com>
2022-09-19 15:39:37 -0500

    File transfer complete

--- client/client.py
+++ client/client.py
@@ -1,5 +1,9 @@
 import socket
 import threading
+from tkinter.tix import FileEntry
+from tqdm import tqdm
+import os
+
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 6969
 ADDR = (IP, PORT)
@@ -14,9 +18,35 @@ def connect_client(client_num):
     while connected:
         client.send(f"!CONNECTION {client_num}".encode(FORMAT))
         msg = client.recv(1024).decode(FORMAT)
+        print(f"[SERVER] {msg}")
         if msg == "!DISCONNECT":
             connected = False
-        print(f"[SERVER] {msg}")
+        elif msg == "OK":
+            print(f"[CLIENT #{client_num}] Connection established waiting for file transfer")
+            client.send("OK".encode(FORMAT))
+            msg = client.recv(1024).decode(FORMAT)
+            if "TRANSFER" in msg:
+                file_data = msg.split(":")
+                filename = file_data[1].rstrip()
+                filesize = int(file_data[2])
+                bar = tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+                try:
+                    os.makedirs('./client/files')
+                except FileExistsError:
+                    pass
+                client.settimeout(5)
+                try:
+                    with open(f"./client/files/{filename}", "wb") as f:
+                        while True:
+                            data = client.recv(1024)
+                            bar.update(len(data))
+                            if not data:
+                                break
+                            f.write(data)
+                except TimeoutError:
+                    if os.path.getsize(f"./client/files/{filename}") == filesize:
+                        print(f"[CLIENT #{client_num}] File transfer complete")
+                connected = False
     client.close()
 
 def main():
@@ -28,18 +58,25 @@ def main():
     msg = client.recv(1024).decode(FORMAT)
     print(f"[SERVER] {msg}")
 
+    threads = list()
+
+    num_clients = None
     while connected:
         msg = input("Enter a command: ")
         client.send(msg.encode(FORMAT))
+        msg_rcv = client.recv(1024).decode(FORMAT)
+        print(f"[SERVER] {msg_rcv}")
         if msg == "!DISCONNECT":
             connected = False
-        elif msg == "!START_CONN":
-            for i in range(26):
+        elif "!CONFIG" in msg and "Config set" in msg_rcv:
+            num_clients = int(msg.split(':')[2])
+        if msg_rcv == "File Transfer Command Started":
+            for i in range(num_clients):
                 client = threading.Thread(target=connect_client, args=(i+1,))
+                threads.append(client)
                 client.start()
+            connected = False
 
-        msg = client.recv(1024).decode(FORMAT)
-        print(f"[SERVER] {msg}")
         
 
 if __name__ == "__main__":
--- server/classes/client_handling.py
+++ server/classes/client_handling.py
@@ -2,6 +2,7 @@ import os
 import logging
 from logger.logger import exception_to_log
 import asyncio
+from tqdm import tqdm
 
 FORMAT = 'utf-8'
 MENU = "Server Commands:\n!LIST - List all the available files\n!CONFIG - Set up the server file transfer configuration\n!START - Start the file transfer to all clients\n!DISCONNECT - Disconnect from the server\n"
@@ -10,17 +11,19 @@ log = logging.getLogger('TCPServer')
 class Client:
     def __init__(self, connection_id, threading_condition):
         self.connected = False
-        self.connnection = None
+        self._connnection = None
         self.address = None
         self.connection_id = connection_id
         self.conn_type = None
         self.status = None
         self.condition = threading_condition
+        self.selected_file = None
+        self.num_clients = 0
 
     def handle_client(self, conn, addr):
         log.info(f"[NEW CONNECTION] {addr} connected.")
         self.connected = True
-        self.connection = conn
+        self._connection = conn
         self.address = addr
         config_msg = conn.recv(1024).decode(FORMAT)
         if config_msg == "!MAIN_CONN":
@@ -28,26 +31,28 @@ class Client:
             self.conn_type = "main"
         elif "!CONNECTION" in config_msg:
             self.conn_type = "client"
-        self.selected_file = None
-        self.num_clients = 0
+            self.send("OK")
         while self.connected:
             try:
                 if self.conn_type == "main":
-                   self.main_client()           
+                   self.main_client()       
                 elif self.conn_type == "client":
                     self.normal_client()
             except Exception as e:
-                self.send("Internal Server Error")
+                try:
+                    self.send("Internal Server Error")
+                except ConnectionResetError:
+                    pass
                 exception_to_log(log, e)
-        self.connection.close()
+        self._connection.close()
 
-    def send(self, msg, log=False):
-        self.connection.send(msg.encode(FORMAT))
-        if log:
+    def send(self, msg, log_msg=False):
+        self._connection.send(msg.encode(FORMAT))
+        if log_msg:
             log.info(f"[FILE TRANSFER] - {msg}")
 
     def main_client(self):
-        msg = self.connection.recv(1024).decode(FORMAT)
+        msg = self._connection.recv(1024).decode(FORMAT)
         files = [f for f in os.listdir("./server/files") if os.path.isfile(os.path.join("./server/files", f))]
         if msg == "!DISCONNECT":
             self.connected = False
@@ -57,21 +62,22 @@ class Client:
         elif msg == "!LIST":
             self.send(str(files))
         elif "!CONFIG" in msg and (self.selected_file == None or self.num_clients == 0):
-            commands = msg.split()
+            commands = msg.split(':')
             if len(commands) == 1:
-                self.send("Invalid cPlease use the following format: !CONFIG <file> <num_clients>onfig format (Empty config). Please use the following format: !CONFIG <file> <num_clients>")
+                self.send("Invalid config format (Empty config). Please use the following format: !CONFIG :<file> :<num_clients>")
             elif len(commands) != 3:
                 msg_str = ""
                 for string in commands:
                     msg_str += string + " "
-                self.send(f"Invalid config format! {msg_str}. Please use the following format: !CONFIG <file> <num_clients>")
+                self.send(f"Invalid config format! {msg_str}. Please use the following format: !CONFIG :<file> :<num_clients>")
                 return
-            elif commands[1] not in files:
+            elif commands[1].rstrip() not in files:
                 self.send(f"File {commands[1]} does not exist")
                 return
             try:
                 self.num_clients = int(commands[2])
                 self.selected_file = commands[1]
+                self.send(f"Config set to {commands[1]} and {commands[2]} clients")
             except ValueError:
                 self.send(f"Invalid number of clients: {commands[2]}")
         elif msg == "!CONFIG":
@@ -82,24 +88,37 @@ class Client:
             if not self.selected_file or self.num_clients == 0:
                 self.send("The settings have not been set. You need to configure the server in order to start")
             else:
-                self.send("File Transfer Command Started", log=True)
-                while (len(self._clientList) != self.num_clients):
+                self.send("File Transfer Command Started", log_msg=True)
+                while (len(self._clientList) <= self.num_clients):
                     asyncio.run(asyncio.sleep(1))
+                for client in self._clientList:
+                    if client.conn_type == "client":
+                        client.selected_file = self.selected_file
+                self.condition.acquire()
                 self.condition.notify_all()
+                self.condition.release()
+                self.connected = False # Disconnect the main client
         else:
             self.send("Invalid command\n"+MENU)
 
     def normal_client(self):
-        msg = self.connection.recv(1024).decode(FORMAT)
+        msg = self._connection.recv(1024).decode(FORMAT)
         if msg == "!DISCONNECT":
             self.connected = False
             self.send("Disconnected from server")
             log.info(f"[DISCONNECTED] {self.address} disconnected.")
         elif msg == "OK":
             self.status = "OK"
+            self.condition.acquire()
             self.condition.wait()
-            ## TODO: Send file to client
-            self.send("OK")
+            self.condition.release()
+            self.send(f"TRANSFER :{self.selected_file}:{os.path.getsize(f'./server/files/{self.selected_file}')}")
+            with open(f"./server/files/{self.selected_file}", "rb") as f:
+                data = f.read()
+            self._connection.sendall(data)
+            log.info("File sent to client")
+
+            
 
 
     def setClientList(self, list):
