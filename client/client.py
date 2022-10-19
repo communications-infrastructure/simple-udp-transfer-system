6c5565a30b6f9a7f20c1fda5621c6cf5b8b519ac
ElReyZero <juan.romero1201@gmail.com>
2022-10-19 11:22:03 -0500

    Add: Client functionality (UDP)

--- client/client.py
+++ client/client.py
@@ -4,9 +4,10 @@ from tqdm import tqdm
 import os
 import sys
 import logging
-import time
+import select
 from hashfinder.get_hash import hash_file
 from logger.logger import define_log, StreamToLogger
+from server.server import PROJECT_PATH
 
 log = logging.getLogger("client")
 IP = socket.gethostbyname(socket.gethostname())
@@ -16,7 +17,6 @@ FORMAT = 'utf-8'
 COMMANDS = ["!DISCONNECT", "!CONFIG", "!START", "!LIST"]
 MENU = "Server Commands:\n!LIST - List all the available files\n!CONFIG - Set up the server file transfer configuration\n!START - Start the file transfer to all clients\n!DISCONNECT - Disconnect from the server\n"
 num_clients = None
-
 
 def setup_log():
     console_handler, file_handler = define_log()
@@ -28,83 +28,43 @@ def setup_log():
 
 
 def connect_client(client_num):
+    timeout = 5
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client.sendto(f"!CONNECTION {client_num}".encode(FORMAT), ADDR)
-    connected = True
     log.info(f"[CONNECTED] Client #{client_num} - Connected to {IP}:{PORT}")
-
-    while connected:
-        msg = client.recvfrom(1024)[0].decode(FORMAT)
-        log.info(f"[SERVER] {msg}")
-        if msg == "!DISCONNECT":
-            connected = False
-        elif msg == "OK":
-            log.info(
-                f"[CLIENT #{client_num}] Connection established waiting for file transfer")
-            client.sendto("OK".encode(FORMAT), ADDR)
-            msg = client.recvfrom(1024)[0].decode(FORMAT)
-            if "TRANSFER" in msg:
-                hash = client.recvfrom(1024)[0].decode(FORMAT)
-                file_data = msg.split(":")
-                filename = f"File {client_num} " + file_data[1].rstrip()
-                filesize = int(file_data[2])
-                log.info(
-                    f"[CLIENT #{client_num}] Receiving file {filename} with size {filesize}")
-                bar = tqdm(range(
-                    filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-                try:
-                    os.makedirs('./client/ArchivosRecibidos')
-                except FileExistsError:
-                    pass
-                global num_clients
-
-                with open(f"./client/ArchivosRecibidos/Cliente{client_num}-Prueba{num_clients}.mp4", "wb") as f:
-                    try:
-                        t1 = time.time()
-                        while True:
-                            data = client.recvfrom(1024)[0]
-                            if len(data) <= 0:
-                                bar.close()
-                                t2 = time.time()
-                                log.info(
-                                    f"[CLIENT #{client_num}] File transfer time: {t2-t1} seconds")
-                                break
-                            bar.update(len(data))
-                            f.write(data)
-                    except (ConnectionResetError, ConnectionAbortedError, ConnectionRefusedError):
-                        log.error("ERROR: Connection lost")
-                        connected = False
-                log.info(f"[CLIENT #{client_num}] File transfer complete")
-                clientHash = hash_file(
-                    f"./client/ArchivosRecibidos/Cliente{client_num}-Prueba{num_clients}.mp4")
-                if hash == clientHash:
-                    log.info(
-                        f"[CLIENT #{client_num}] File transfer successful, integrity check passed - Hashes are equal.")
-                    log.info(f"File Size: {filesize} bytes")
-                    log.info(f"Server Hash: {hash}")
-                    log.info(f"Client Hash: {clientHash}")
+    try:
+        os.makedirs(PROJECT_PATH+'/client/ArchivosRecibidos')
+    except FileExistsError:
+        pass
+    client.sendto(f"TRANSFER".encode(FORMAT), ADDR)
+    data, addr = client.recvfrom(1024)
+    if data:
+        file_name = data.strip()
+        with open(PROJECT_PATH + f"/client/ArchivosRecibidos/Cliente{client_num}-Prueba{num_clients}.mp4") as f:
+            receiving = True
+            while receiving:
+                receiving = select.select([client], [], [], timeout)[0]
+                if receiving:
+                    data, addr = client.recvfrom(65536)
+                    f.write(data)
                 else:
-                    log.info(
-                        f"[CLIENT #{client_num}] File transfer successful, integrity check failed - Hashes are not equal.")
-                    log.info(f"File Size: {filesize} bytes")
-                    log.info(f"Server Hash: {hash}")
-                    log.info(f"Client Hash: {clientHash}")
+                    f.close()
+                    receiving = False
+                    log.info(f"[RECEIVED] Client #{client_num} - File received")
 
-                connected = False
-    client.close()
 
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    log.info(f"[STARTING] Client is starting...")
     config = False
+    global num_clients
     msg = MENU.split("\n")
+    log.info(f"[STARTING] Client is starting...")
     for i in msg:
         log.info(f"[MENU] {i}")
 
     threads = list()
     connected = True
-    global num_clients
+
     while connected:
         msg = input("[MENU] Enter a command: ")
         if not msg in COMMANDS and not "!CONFIG" in msg:
@@ -115,13 +75,17 @@ def main():
             data, addr = client.recvfrom(1024)
             log.info(f"[SERVER] {data.decode(FORMAT)}")
         elif "!CONFIG" in msg and not config:
-            client.sendto(msg.encode(FORMAT), addr)
-            config = True
+            client.sendto(msg.encode(FORMAT), ADDR)
+            data, addr = client.recvfrom(1024)
+            if data.decode(FORMAT) == "Config set":
+                config = True
+                num_clients = msg.split(":")[2]
+            log.info(f"[SERVER] {data.decode(FORMAT)}")
         elif "!CONFIG" in msg:
             log.info("[MENU] Server already configured")
         elif "!DISCONNECT" == msg:
             sys.exit(0)
-            # TODO: Connect
+        elif "!START" == msg:
             for i in range(num_clients):
                 client = threading.Thread(target=connect_client, args=(i+1,))
                 threads.append(client)
--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 from logger.logger import define_log, StreamToLogger
-from classes.client_handling import Client
+import asyncio
 import socket
 import threading
 import os
@@ -32,8 +32,14 @@ def setup_log():
 class SocketListener(threading.Thread):
 
     def run(self):
-        config = None
+        ## Server Variables
+        file_to_be_sent = None
+        path = PROJECT_PATH + "/files"
+        if not "/server" in path:
+            path = PROJECT_PATH + "/server/files"
         files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+        # Server config
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         server.bind(ADDR)
         log.info(f"[PROCESS] Server process ID: {os.getpid()}")
@@ -43,25 +49,31 @@ class SocketListener(threading.Thread):
             data = data.decode(FORMAT)
             log.info(f"[RECEIVED] Received data from client: {data}")
             if "LIST" == data:
-                path = PROJECT_PATH + "/files"
-                if not "/server" in path:
-                    path = PROJECT_PATH + "/server/files"
                 server.sendto(str(files).encode(FORMAT), addr)
             elif "CONFIG" in data:
                 commands = data.split(':')
-                if len(commands) == 1:
+                if len(commands) == 1 or len(commands) == 0:
                     server.sendto("Invalid config format (Empty config). Please use the following format: !CONFIG :<file>".encode(FORMAT), addr)
                 elif len(commands) != 3:
                     msg_str = ""
                     for string in commands:
                         msg_str += string + " "
                     server.sendto(f"Invalid config format! {msg_str}. Please use the following format: !CONFIG :<file> :<num_clients>".encode(FORMAT), addr)
-                    return
                 elif commands[1].rstrip() not in files:
-                    server.send(f"File {commands[1].rstrip()} does not exist".encode(FORMAT), addr)
-                    return
+                    server.sendto(f"File {commands[1].rstrip()} does not exist".encode(FORMAT), addr)
+                else:
+                    file_to_be_sent = commands[1].rstrip()
+                    server.sendto("Config set".encode(FORMAT), addr)
             elif "TRANSFER" == data:
-                pass
+                with open(file_to_be_sent, 'rb') as f:
+                    log.info(f"[TRANSFER] Sending file {file_to_be_sent} to client")
+                    data = f.read(65536)
+                    while data:
+                        if server.sendto(data, addr):
+                            data = f.read(65536)
+                            asyncio.sleep(0.02)
+
+                log.info(f"[TRANSFER] File {file_to_be_sent} sent to client")
 
 def main():
     log.info("[STARTING] server is starting...")
